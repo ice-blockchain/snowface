dabe25a310b7342bde1be4f8e59b750514a982fa
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2023-10-17 17:16:08 +0300

    metrics with success / failures per liveness session, successes for primary photo

--- api/metrics.py
+++ api/metrics.py
@@ -15,6 +15,9 @@ _frames_scores_with_emotion = Histogram("emotion_frames_scores", "Summary with s
 _emotion_avg_score = Histogram("emotion_avg_score", "Summary with avg scores by requested emotion", labelnames=["expected_emotion", "actual_emotion"], buckets=[i*5 for i in range(20)])
 _emotions_success = Counter("emotion_success", f"Counter of successfully passed emotion liveness checks for emotion", labelnames=["expected_emotion"])
 _emotions_failure = Counter("emotion_failure", f"Counter of failed emotion liveness checks for emotion", labelnames=["expected_emotion"])
+_emotion_session_success = Counter("emotion_session_success","Counter of successfully passed emotion liveness sessions")
+_emotion_session_failure = Counter("emotion_session_failure","Counter of failed emotion liveness sessions")
+
 _session_length = None
 _gunicorn_queue = Histogram("gunicorn_queue_request_time", "Time per request spent in queue", labelnames=["path"])
 REQUEST_TIME = Histogram('request_processing_seconds', 'Time spent processing request', labelnames=["path"])
@@ -23,6 +26,7 @@ _similarity_failure_distance_sface = Histogram("similarity_failure_distance_sfac
 _similarity_failure_distance_arcface = Histogram("similarity_failure_distance_arcface", "Euclidian (L2) distances of faces between primary users photo and failed secondary (for fallback model: arcface)", buckets=(1.1,1.15,1.2,1.25,1.3,1.5,1.75,2.0))
 _disabled_user_similarity_sface = Histogram("disabled_user_similarity_sface", "Euclidian (L2) distances of faces between most similar user and disabled one (for primary model: sface)", buckets=(0.1,0.2,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,1.06))
 _disabled_user_similarity_arcface = Histogram("disabled_user_similarity_arcface", "Euclidian (L2) distances of faces between most similar user and disabled one (for fallback model: arcface)", buckets=(0.1,0.2,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,1.05,1.1,1.15))
+_primary_photo_passed = Counter("primary_photo_passed","Counter of successfully passed primary photo uploads (not disabled)")
 def register_emotion_success(model: HSEmotionRecognizer, emotion: str, scores_by_frame: list, averages: dict):
     _emotions_success.labels(expected_emotion=emotion).inc()
     _update_frames(model, emotion,scores_by_frame, averages)
@@ -35,6 +39,9 @@ def register_session_length(length:int):
     if not _session_length:
         _session_length = Histogram("emotion_session_length", "Length of the session", buckets=[i for i in range(current_app.config['MAX_EMOTION_COUNT'])])
     _session_length.observe(length)
+    _emotion_session_success.inc()
+def register_session_failure():
+    _emotion_session_failure.inc()
 
 def register_gunicorn_latency(path, latency):
     _gunicorn_queue.labels(path).observe(latency)
@@ -54,3 +61,5 @@ def register_disabled_user(sface_distance, arcface_distance):
     _disabled_user_similarity_sface.observe(sface_distance)
     if arcface_distance != -1:
         _disabled_user_similarity_arcface.observe(arcface_distance)
+def register_primary_photo_uploaded():
+    _primary_photo_passed.inc()
