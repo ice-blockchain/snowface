d0ff5b2242c75d541b47a8f201f233c1fe1d92eb
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-01-20 19:54:24 +0300

    fix delete call for non-existing users

--- api/service.py
+++ api/service.py
@@ -881,7 +881,9 @@ def proxy_delete(current_user, user_id = ""):
     if not user_id:
         user = _get_user(current_user.user_id)
         if user is None:
-            return "", 204
+            primary_photo = _get_primary_metadata(user_id,model=_model_fallback, search_growing=False)
+            if not primary_photo:
+                return "", 204
     response = requests.delete(
         url=url,
         headers={
