79ecbc0d3c23800881a8db59098784797da69d96
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-01-20 19:37:22 +0300

    fix delete call for non-existing users

--- api/service.py
+++ api/service.py
@@ -874,10 +874,14 @@ def proxy_delete(current_user, user_id = ""):
 
     url = f"{similarity_server[:-1] if similarity_server.endswith('/') else similarity_server}/v1w/face-auth/"
     payload = None
+
     if user_id != "":
         url = f"{url}?userId={user_id}"
         payload = {"userId":user_id}
-
+    if not user_id:
+        user = _get_user(current_user.user_id)
+        if user is None:
+            return "", 204
     response = requests.delete(
         url=url,
         headers={
